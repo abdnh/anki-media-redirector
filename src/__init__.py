cc05b93efa3f2f9bad4eec896d2f609959770e65
Abdo <abdo@abdnh.net>
2023-01-08 07:23:13 +0300

    Add option to remove redirected media

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,20 @@
 from __future__ import annotations
 
 import re
+from pathlib import Path
 from typing import Any
 
 from anki import hooks
+from anki.collection import Collection
 from aqt import mediasrv, mw
 from aqt.mediasrv import BundledFileRequest, DynamicRequest, LocalFileRequest, NotFound
+from aqt.operations import QueryOp
+from aqt.qt import *
 
+ADDON_NAME = "Media Redirector"
 CONFIG = mw.addonManager.getConfig(__name__)
 redirected_patterns = [re.compile(p) for p in CONFIG["redirected_patterns"]]
+REDIRECTED_MEDIA_DIR = Path(__file__).parent / "user_files" / "media"
 
 
 def should_redirect(path: str) -> bool:
@@ -27,7 +33,26 @@ def redirect_media_request(
     return _old(path)
 
 
+def remove_redirected() -> None:
+    def op(col: Collection) -> None:
+        for path in Path(col.media.dir()).iterdir():
+            if (
+                path.is_file()
+                and should_redirect(path.name)
+                and (REDIRECTED_MEDIA_DIR / path.name).exists()
+            ):
+                os.remove(path)
+
+    QueryOp(parent=mw, op=op, success=lambda _: ()).run_in_background()
+
+
 mw.addonManager.setWebExports(__name__, r"user_files/media/.*")
 mediasrv._extract_request = hooks.wrap(
     mediasrv._extract_request, redirect_media_request, "around"
 )
+
+menu = QMenu(ADDON_NAME)
+action = QAction("Remove redirected files from collection.media")
+qconnect(action.triggered, remove_redirected)
+menu.addAction(action)
+mw.form.menuTools.addMenu(menu)
